97abb653817c2c01764f76e5cf44e55359e833d5
AntonBabenko96 <ababenko159@gmail.com>
2023-02-24 00:02:37 +0200

    qqq

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from 'components/Statistics/Statistics';
 import { Section } from '../components/Section/Section';
@@ -6,48 +6,91 @@ import { Notification } from './Notification/Notification';
 
 import css from '../components/App.module.css';
 
-export class App extends Component {
-  state = {
+export const App = () => {
+  const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  leaveFeedback = name => {
-    this.setState(prevState => {
+  const leaveFeedback = name => {
+    setState(prevState => {
       const value = prevState[name];
-      return {
-        [name]: value + 1,
-      };
+      return { ...prevState, [name]: value + 1 };
     });
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positivePercentage = Math.round((good / total) * 100) || 0;
-    return (
-      <div className={css.section}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            leaveFeedback={this.leaveFeedback}
-            options={Object.keys(this.state)}
+  const { good, neutral, bad } = state;
+  const total = good + neutral + bad;
+  const positivePercentage = Math.round((good / total) * 100) || 0;
+
+  return (
+    <div className={css.section}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          leaveFeedback={leaveFeedback}
+          options={Object.keys(state)}
+        />
+      </Section>
+      <Section title="Statistics">
+        {total !== 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {total !== 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification message={'No feedback given'} />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message={'No feedback given'} />
+        )}
+      </Section>
+    </div>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   leaveFeedback = name => {
+//     this.setState(prevState => {
+//       const value = prevState[name];
+//       return {
+//         [name]: value + 1,
+//       };
+//     });
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const total = good + neutral + bad;
+//     const positivePercentage = Math.round((good / total) * 100) || 0;
+//     return (
+//       <div className={css.section}>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             leaveFeedback={this.leaveFeedback}
+//             options={Object.keys(this.state)}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {total !== 0 ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={total}
+//               positivePercentage={positivePercentage}
+//             />
+//           ) : (
+//             <Notification message={'No feedback given'} />
+//           )}
+//         </Section>
+//       </div>
+//     );
+//   }
+// }
